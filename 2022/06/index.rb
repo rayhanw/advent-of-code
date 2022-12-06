f0f71b8e3900d362c00bcd4556b04d70bed03f9c
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-12-06 19:07:41 +0700

    move to method

--- 2022/06/index.rb
+++ 2022/06/index.rb
@@ -1,25 +1,45 @@
-file = File.readlines("input.txt").map(&:strip)[0]
-set = file.chars
+FILE = File.readlines("input.txt").map(&:strip)[0]
+SET = FILE.chars
 
-## PART 1
-mapped = set.map.with_index do |char, index|
-  [
-    *set[index..(index + 2)],
-    set[index + 3]
-  ]
+def packet_finder(character_count)
+  low_limit = character_count - 2
+  top_limit = character_count - 1
+  mapped = SET.map.with_index do |char, index|
+    [
+      *SET[index..(index + low_limit)],
+      SET[index + top_limit]
+    ]
+  end
+  first = mapped.find { |group| group.uniq.length == character_count }
+  mapped.index(first) + 1 + top_limit
 end
-first = mapped.find { |group| group.uniq.length == 4 }
-p first
-p mapped.index(first) + 1 + 3
+
+# PART 1
+p packet_finder(4)
+
+# PART 2
+p packet_finder(14)
+
+### Separate attempts
+## PART 1
+# mapped = set.map.with_index do |char, index|
+#   [
+#     *set[index..(index + 2)],
+#     set[index + 3]
+#   ]
+# end
+# first = mapped.find { |group| group.uniq.length == 4 }
+# p first
+# p mapped.index(first) + 1 + 3
 
 ## PART 2
-mapped = set.map.with_index do |char, index|
-  [
-    *set[index..(index + 12)],
-    set[index + 13]
-  ]
-end
+# mapped = set.map.with_index do |char, index|
+#   [
+#     *set[index..(index + 12)],
+#     set[index + 13]
+#   ]
+# end
 
-first = mapped.find { |group| group.uniq.length == 14 }
-p first
-p mapped.index(first) + 1 + 13
+# first = mapped.find { |group| group.uniq.length == 14 }
+# p first
+# p mapped.index(first) + 1 + 13
