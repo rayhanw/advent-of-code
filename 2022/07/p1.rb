869cda00ec0fd5feed15c684fca4c8c1f54ab8b3
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-12-07 13:07:50 +0700

    add rubocop and structure folder

--- 2022/07/p1.rb
+++ 2022/07/p1.rb
@@ -1,27 +1,27 @@
 file = File.readlines("input.txt").map(&:strip)
-COMMAND_PREFIX = "$"
 file_system = { "/" => { "contents" => [] } }
 groups = file.slice_before { |ele| ele.match?(/cd/) }.to_a
 
-groups[0..0].each do |dir|
-  directory = dir[0].gsub("$ cd ", '')
-  contents = dir[1..].reject { |txt| txt[0] == "$" }
-  contents.each do |content|
-    structure = content.split(" ")
-    if structure[0] == "dir"
-      file_system[directory][structure[1]] = {}
-    else
-      size = structure[0]
-      name = structure[1]
-      file_system[directory]["contents"] << { "name" => name, size => size }
+def make_file_system(file_system, groups)
+  groups.each do |dir|
+    directory = dir[0].gsub("$ cd ", '')
+    contents = dir[1..].reject { |txt| txt[0] == "$" }
+    contents.each do |content|
+      structure = content.split(" ")
+      file_system[directory] = { "contents" => [] } if file_system[directory].nil?
+      if structure[0] == "dir"
+        file_system[directory][structure[1]] = {}
+      else
+        size = structure[0]
+        name = structure[1]
+        file_system[directory]["contents"] << { name => size }
+      end
     end
   end
+
+  file_system
 end
 
-# groups[1..1].each do |dir|
-#   directory = dir[0].gsub("$ cd ", '')
-#   contents = dir[1..].reject { |txt| txt[0] == "$" }
-#   puts "dir: #{directory}"
-# end
+make_file_system(file_system, groups)
 
 pp file_system
