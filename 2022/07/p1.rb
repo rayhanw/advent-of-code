3395fc6c57cb3b268a5d7accf4135bf0d473f1e6
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-12-07 13:48:13 +0700

    redo

--- 2022/07/p1.rb
+++ 2022/07/p1.rb
@@ -2,6 +2,16 @@ file = File.readlines("input.txt").map(&:strip)
 file_system = { "/" => { "contents" => [] } }
 groups = file.slice_before { |ele| ele.match?(/cd/) }.to_a
 
+def nested_hash_value(obj, key)
+  if obj.respond_to?(:key?) && obj.key?(key)
+    obj[key]
+  elsif obj.respond_to?(:each)
+    r = nil
+    obj.find { |*a| r = nested_hash_value(a.last, key) }
+    r
+  end
+end
+
 def make_file_system(file_system, groups)
   groups.each do |dir|
     directory = dir[0].gsub("$ cd ", '')
@@ -24,4 +34,18 @@ end
 
 make_file_system(file_system, groups)
 
-pp file_system
+main_dir = file_system["/"]
+pp main_dir
+puts
+
+# until file_system.keys.length == 1
+  # Grabs main directory
+  main_dir = file_system["/"]
+  # Grabs ever other directories that should be found somewhere within and replaced with
+  rest_dir = file_system.reject { |k, _| k == "/" }
+  rest_dir.each do |k, v|
+    p k
+    p v.reject { |k2, _| k2 == "contents" }
+    puts
+  end
+# end
