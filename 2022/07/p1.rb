4c902555ab1656d2935093947d81584606360dea
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-12-07 12:31:57 +0700

    structuring folders in 07

--- 2022/07/p1.rb
+++ 2022/07/p1.rb
@@ -1,3 +1,27 @@
 file = File.readlines("input.txt").map(&:strip)
+COMMAND_PREFIX = "$"
+file_system = { "/" => { "contents" => [] } }
+groups = file.slice_before { |ele| ele.match?(/cd/) }.to_a
 
-p file
+groups[0..0].each do |dir|
+  directory = dir[0].gsub("$ cd ", '')
+  contents = dir[1..].reject { |txt| txt[0] == "$" }
+  contents.each do |content|
+    structure = content.split(" ")
+    if structure[0] == "dir"
+      file_system[directory][structure[1]] = {}
+    else
+      size = structure[0]
+      name = structure[1]
+      file_system[directory]["contents"] << { "name" => name, size => size }
+    end
+  end
+end
+
+# groups[1..1].each do |dir|
+#   directory = dir[0].gsub("$ cd ", '')
+#   contents = dir[1..].reject { |txt| txt[0] == "$" }
+#   puts "dir: #{directory}"
+# end
+
+pp file_system
