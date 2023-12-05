c4dd5c01e89bcf7a0925044421871b104255539d
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2023-12-05 17:49:14 +0800

    01

--- 2023/05/helpers.rb
+++ 2023/05/helpers.rb
@@ -5,23 +5,15 @@ def generate_map(file, key)
   maps = file[(start_index + 1)..]
   end_index = maps.index('')
   maps = maps[0...end_index]
-  result = maps.map do |map|
+  maps.map do |map|
     list = map.split(' ')
-    source_values = (list[1].to_i..(list[1].to_i + list[2].to_i)).to_a
-    destination_values = (list[0].to_i..(list[0].to_i + list[2].to_i)).to_a
     {
       destination: list[0],
       source: list[1],
       range: list[2],
-      source_values:,
-      destination_values:
+      limit: list[0].to_i + list[2].to_i
     }
   end
-
-  sources = result.map { |r| r[:source_values] }.flatten
-  destinations = result.map { |r| r[:destination_values] }.flatten
-
-  { sources:, destinations: }
 end
 
 def print_map(map, title)
--- 2023/05/solution.rb
+++ 2023/05/solution.rb
@@ -1,92 +1,136 @@
 require 'benchmark'
+require 'colorized_string'
 require_relative '../../helpers/advent_of_code'
 require_relative 'helpers'
 
 aoc = Helpers::AdventOfCode.new(File.join(__dir__, 'input.txt'))
 file = aoc.file
 seeds = file[0].gsub('seeds: ', '').split(' ').map(&:to_i)
-puts "--- Generated seeds ---"
 
 ### Seed to Soil ###
 seed_to_soil_map = generate_map(file, 'seed-to-soil map:')
-# print_map(seed_to_soil_map, 'Seed to Soil')
-puts "--- Generated seed to soil map ---"
-
 ### Soil to Fertilizer ###
 soil_to_fertilizer_map = generate_map(file, 'soil-to-fertilizer map:')
-# print_map(soil_to_fertilizer_map, 'Soil to Fertilizer')
-puts "--- Generated soil to fertilizer map ---"
-
 ### Fertilizer to Water ###
 fertilizer_to_water_map = generate_map(file, 'fertilizer-to-water map:')
-# print_map(fertilizer_to_water_map, 'Fertilizer to Water')
-puts "--- Generated fertilizer to water map ---"
-
 ### Water to Light ###
 water_to_light_map = generate_map(file, 'water-to-light map:')
-# print_map(water_to_light_map, 'Water to Light')
-puts "--- Generated water to light map ---"
-
 ### Light to Temperature ###
 light_to_temperature_map = generate_map(file, 'light-to-temperature map:')
-# print_map(light_to_temperature_map, 'Light to Temperature')
-puts "--- Generated light to temperature map ---"
-
 ### Temperature to Humidity ###
 temperature_to_humidity_map = generate_map(file, 'temperature-to-humidity map:')
-# print_map(temperature_to_humidity_map, 'Temperature to Humidity')
-puts "--- Generated temperature to humidity map ---"
-
 ### Humidity to Location ###
 humidity_to_location_map = generate_map(file, 'humidity-to-location map:')
-# print_map(humidity_to_location_map, 'Humidity to Location')
-puts "--- Generated humidity to location map ---"
 
-puts "Seeds: #{seeds}"
-complete_info = []
+locations = []
+
 seeds.each do |seed|
-  info = {}
-  # Seed to Soil
-  soil_index = seed_to_soil_map[:sources].index(seed)
-  soil = seed
-  soil = seed_to_soil_map[:destinations][soil_index] if soil_index
-  # Soil to Fertilizer
-  fertilizer_index = soil_to_fertilizer_map[:sources].index(soil)
-  fertilizer = soil
-  fertilizer = soil_to_fertilizer_map[:destinations][fertilizer_index] if fertilizer_index
-  # Fertilizer to Water
-  water_index = fertilizer_to_water_map[:sources].index(fertilizer)
-  water = fertilizer
-  water = fertilizer_to_water_map[:destinations][water_index] if water_index
-  # Water to Light
-  light_index = water_to_light_map[:sources].index(water)
-  light = water
-  light = water_to_light_map[:destinations][light_index] if light_index
-  # Light to Temperature
-  temperature_index = light_to_temperature_map[:sources].index(light)
-  temperature = light
-  temperature = light_to_temperature_map[:destinations][temperature_index] if temperature_index
-  # Temperature to Humidity
-  humidity_index = temperature_to_humidity_map[:sources].index(temperature)
-  humidity = temperature
-  humidity = temperature_to_humidity_map[:destinations][humidity_index] if humidity_index
-  # Humidity to Location
-  location_index = humidity_to_location_map[:sources].index(humidity)
-  location = humidity
-  location = humidity_to_location_map[:destinations][location_index] if location_index
+  info = { seed: }
 
-  info[:seed] = seed
-  info[:soil] = soil
-  info[:fertilizer] = fertilizer
-  info[:water] = water
-  info[:light] = light
-  info[:temperature] = temperature
-  info[:humidity] = humidity
-  info[:location] = location
-  complete_info << info
-  puts "Seed #{seed}, soil #{soil}, fertilizer #{fertilizer}, water #{water}, light #{light}, temperature #{temperature}, humidity #{humidity}, location #{location}"
+  # Seed to Soil
+  potential_soils = []
+  seed_to_soil_map.each do |map|
+    diff = seed.to_i - map[:source].to_i
+    if diff.negative?
+      info[:soil] = seed.to_i
+      next
+    end
+    pot = diff + map[:destination].to_i
+    info[:soil] = pot > map[:limit] ? seed.to_i : pot
+    potential_soils << { diff:, value: info[:soil] }
+  end
+  info[:soil] = potential_soils.min_by { |i| i[:diff] }[:value] if potential_soils.length.positive?
+
+  # Soil to Fertilizer
+  potential_fertilizers = []
+  soil_to_fertilizer_map.each do |map|
+    diff = info[:soil] - map[:source].to_i
+    if diff.negative?
+      info[:fertilizer] = info[:soil]
+      next
+    end
+    pot = diff + map[:destination].to_i
+    info[:fertilizer] = pot > map[:limit] ? info[:soil] : pot
+    potential_fertilizers << { diff:, value: info[:fertilizer] }
+  end
+  info[:fertilizer] = potential_fertilizers.min_by { |i| i[:diff] }[:value] if potential_fertilizers.length.positive?
+
+  # Fertilizer to Water
+  potential_waters = []
+  fertilizer_to_water_map.each do |map|
+    diff = info[:fertilizer] - map[:source].to_i
+    if diff.negative?
+      info[:water] = info[:fertilizer]
+      next
+    end
+    pot = diff + map[:destination].to_i
+    info[:water] = pot > map[:limit] ? info[:fertilizer] : pot
+    potential_waters << { diff:, value: info[:water] }
+  end
+  info[:water] = potential_waters.min_by { |i| i[:diff] }[:value] if potential_waters.length.positive?
+
+  # Water to Light
+  potential_lights = []
+  water_to_light_map.each do |map|
+    diff = info[:water] - map[:source].to_i
+    if diff.negative?
+      info[:light] = info[:water]
+      next
+    end
+
+    pot = diff + map[:destination].to_i
+    info[:light] = pot > map[:limit] ? info[:water] : pot
+    potential_lights << { diff:, value: info[:light] }
+  end
+  info[:light] = potential_lights.min_by { |i| i[:diff] }[:value] if potential_lights.length.positive?
+
+  # Light to Temperature
+  potential_temperatures = []
+  light_to_temperature_map.each do |map|
+    diff = info[:light] - map[:source].to_i
+    if diff.negative?
+      info[:temperature] = info[:light]
+      next
+    end
+
+    pot = diff + map[:destination].to_i
+    info[:temperature] = pot > map[:limit] ? info[:light] : pot
+    potential_temperatures << { diff:, value: info[:temperature] }
+  end
+  info[:temperature] = potential_temperatures.min_by { |i| i[:diff] }[:value] if potential_temperatures.length.positive?
+
+  # Temperature to Humidity
+  potential_humidities = []
+  temperature_to_humidity_map.each do |map|
+    diff = info[:temperature] - map[:source].to_i
+    if diff.negative?
+      info[:humidity] = info[:temperature]
+      next
+    end
+
+    pot = diff + map[:destination].to_i
+    info[:humidity] = pot > map[:limit] ? info[:temperature] : pot
+    potential_humidities << { diff:, value: info[:humidity] }
+  end
+  info[:humidity] = potential_humidities.min_by { |i| i[:diff] }[:value] if potential_humidities.length.positive?
+
+  # Humidity to Location
+  potential_locations = []
+  humidity_to_location_map.each do |map|
+    diff = info[:humidity] - map[:source].to_i
+    if diff.negative?
+      info[:location] = info[:humidity]
+      next
+    end
+
+    pot = diff + map[:destination].to_i
+    info[:location] = pot > map[:limit] ? info[:humidity] : pot
+    potential_locations << { diff:, value: info[:location] }
+  end
+  info[:location] = potential_locations.min_by { |i| i[:diff] }[:value] if potential_locations.length.positive?
+
+  locations << info[:location]
+  puts ColorizedString["Seed: #{info[:seed]}, soil #{info[:soil]}, fertilizer #{info[:fertilizer]}, water #{info[:water]}, light #{info[:light]}, temperature #{info[:temperature]}, humidity #{info[:humidity]}, location #{info[:location]}"].colorize(:light_blue)
 end
 
-sorted = complete_info.sort_by { |i| i[:location] }
-lowest = sorted[0]
-puts "Lowest location: #{lowest[:location]}"
+puts ColorizedString["ANSWER: #{locations.min}"].colorize(:green)
