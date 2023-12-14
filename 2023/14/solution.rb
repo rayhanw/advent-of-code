83676316e9d17d6d32ae964ed4861d811bc85f85
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2023-12-14 14:09:36 +0800

    refactor

--- 2023/14/solution.rb
+++ 2023/14/solution.rb
@@ -9,26 +9,9 @@ modified = []
 file.each do |line|
   puts line.join(" ")
 end
-puts
-file.transpose.each do |line|
-  # line_str = line.join("")
-  # rock_amount = line_str.count("O")
-  # dot_amount = line_str.count(".")
-  # cube_amount = line_str.count("#")
-  # total = rock_amount + dot_amount + cube_amount
-  # puts line.join("")
-  # puts ColorizedString["O: #{rock_amount}"].colorize(:green)
-  # puts ColorizedString[".: #{dot_amount}"].colorize(:blue)
-  # puts ColorizedString["#: #{cube_amount}"].colorize(:cyan)
-  # puts "Total: #{total}"
 
+file.transpose.each do |line|
   modified << bubble_sort(line)
-  # index = 0
-  # until collection.size == line.size
-  #   current = line[index]
-  #   collection << 'x'
-  #   index += 1
-  # end
 end
 
 puts ColorizedString['---- Transposed ----'].colorize(:red)
@@ -36,7 +19,6 @@ sum = 0
 modified.transpose.each_with_index do |line, i|
   rock_amount = line.count('O')
   total = rock_amount * (modified.size - i)
-  puts "#{rock_amount}*#{modified.size - i}=#{total}"
   sum += total
 end
 
