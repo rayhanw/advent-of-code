37800e1b6d0ca2ddbe730656b41c6381f413f9b8
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2024-12-09 14:29:29 +0800

    WIP

--- 2024/09/solution.rb
+++ 2024/09/solution.rb
@@ -1,12 +1,24 @@
-require "benchmark"
-require "colorize"
-require "colorized_string"
+require 'benchmark'
+require 'colorize'
+require 'colorized_string'
 
-FILE = File.readlines("input.txt").map(&:strip)
+FILE = File.readlines('input.txt').map(&:strip)[0]
 
 time = Benchmark.measure do
-  p FILE
+  disk_map = FILE.each_char.map.with_index do |char, idx|
+    amount = char.to_i
+    if idx.even?
+      (idx / 2).to_s * amount
+    else
+      '.' * amount
+    end
+  end
+
+  disk_map = disk_map.join('')
+  puts "Original: #{disk_map} | Length: #{disk_map.length}"
+  reversed = disk_map.reverse.gsub('.', '').chars
+  puts "Reversed: #{reversed.join('')}"
 end
 
-puts "---------------------------------------------".colorize(:yellow)
+puts '---------------------------------------------'.colorize(:yellow)
 puts time
